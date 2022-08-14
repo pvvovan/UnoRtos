a9bb61b5bf03128d73ea6116fd9f06c724d2ba03
PVVovan <pvvovan@gmail.com>
2022-08-14 13:50:28 +0200

    Add header for assembly function

--- Src/gpio.c
+++ Src/gpio.c
@@ -1,4 +1,5 @@
 #include "gpio.h"
+#include "protected_io.h"
 
 #include <avr/io.h>
 
@@ -6,6 +7,7 @@
 void gpio_init(void)
 {
 	DDRB = (1 << 5);
+	// protected_write_io(DDRB, 0, 1 << 5); ???
 }
 
 void gpio_toggle_pin(void)
