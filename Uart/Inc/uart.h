cbe82fe179d016b751199afbec96ba9b1d3da5fd
PVVovan <pvvovan@gmail.com>
2022-08-21 08:53:29 +0200

    Uart0 instance

--- Uart/Inc/uart.h
+++ Uart/Inc/uart.h
@@ -4,19 +4,27 @@
 #include <stddef.h>
 #include <stdint.h>
 
+#include "ring_buffer.h"
+
+
 class uart {
       public:
-	static bool init(uint32_t baud, size_t tx_size, size_t rx_size);
-	static void start();
-	static size_t available();
-	static bool print(const char *str);
-	static bool write(const uint8_t *data, size_t size);
-	static size_t read(uint8_t *data, size_t size);
+	enum class peripheral { uart0, uart1, uart2, uart3 };
+	uart(const peripheral unit, const size_t tx_size, const size_t rx_size);
+	bool init(const uint32_t baud);
+	void start();
+	size_t available();
+	bool print(const char *str);
+	bool write(const uint8_t *data, size_t size);
+	size_t read(uint8_t *data, size_t size);
 
       private:
-	static bool initialized_;
-	static size_t tx_size_;
-	static size_t rx_size_;
+	const peripheral unit_;
+	const size_t tx_size_;
+	const size_t rx_size_;
+	bool initialized_ {false};
+	ring_buffer *tx_buf_ {nullptr};
+	ring_buffer *rx_buf_ {nullptr};
 };
 
 #endif /* UART_H_ */
--- Uart/Src/uart.cpp
+++ Uart/Src/uart.cpp
@@ -1,37 +1,35 @@
 #include "string.h"
 #include <stdlib.h>
 
-#include "ring_buffer.h"
 #include "scope_lock.h"
 #include "uart.h"
 #include "uart_ll.h"
 
-static ring_buffer *tx_buf = nullptr;
-static ring_buffer *rx_buf = nullptr;
+uart::uart(const peripheral unit, const size_t tx_size, const size_t rx_size) :
+	unit_ {unit},
+	tx_size_ {tx_size},
+	rx_size_ {rx_size}
+{ }
 
-bool uart::initialized_{false};
-size_t uart::tx_size_{};
-size_t uart::rx_size_{};
-
-bool uart::init(uint32_t baud, size_t tx_size, size_t rx_size) {
+bool uart::init(const uint32_t baud) {
 	if (initialized_) {
 		return false;
 	}
-	tx_size_ = tx_size;
-	rx_size_ = rx_size;
-	tx_buf = new ring_buffer(tx_size);
-	rx_buf = new ring_buffer(rx_size);
-	const uint32_t ubrr = F_CPU / 16 / baud - 1;
-	::uart0_ll_init(ubrr, rx_buf, tx_buf);
-	initialized_ = true;
-	return true;
+	tx_buf_ = new ring_buffer(tx_size_);
+	rx_buf_ = new ring_buffer(rx_size_);
+	if (tx_buf_ && rx_buf_) {
+		const uint32_t ubrr = F_CPU / 16 / baud - 1;
+		::uart0_ll_init(ubrr, rx_buf_, tx_buf_);
+		initialized_ = true;
+	}
+	return initialized_;
 }
 
 void uart::start() { ::uart_ll_start(); }
 
 size_t uart::available() {
 	scope_lock sl{};
-	size_t avail = rx_buf->count();
+	size_t avail = rx_buf_->count();
 	return avail;
 }
 
@@ -42,16 +40,16 @@ bool uart::print(const char *str) {
 
 bool uart::write(const uint8_t *data, size_t size) {
 	scope_lock sl{};
-	bool empty = tx_buf->count() == 0;
-	if (size >= (tx_size_ - tx_buf->count())) {
+	bool empty = (tx_buf_->count() == 0);
+	if (size >= (tx_size_ - tx_buf_->count())) {
 		return false;
 	}
 	for (size_t i = 0; i < size; i++) {
-		tx_buf->write(data[i]);
+		tx_buf_->write(data[i]);
 	}
-	if (empty && (tx_buf->count() > 0)) {
+	if (empty && (tx_buf_->count() > 0)) {
 		uint8_t b;
-		tx_buf->read(b);
+		tx_buf_->read(b);
 		::uart0_ll_transmit(b);
 	}
 	return true;
@@ -60,11 +58,11 @@ bool uart::write(const uint8_t *data, size_t size) {
 size_t uart::read(uint8_t *data, size_t size) {
 	scope_lock sl{};
 	size_t count = size;
-	if (count > rx_buf->count()) {
-		count = rx_buf->count();
+	if (count > rx_buf_->count()) {
+		count = rx_buf_->count();
 	}
 	for (size_t i = 0; i < count; i++) {
-		rx_buf->read(data[i]);
+		rx_buf_->read(data[i]);
 	}
 	return count;
 }
@@ -75,13 +73,15 @@ extern "C" {
 	void uart_demoprint(const char *str);
 }
 
+static uart s_uart0{uart::peripheral::uart0, 256, 256};
+
 void uart_demoinit()
 {
-	uart::init(9600, 128, 128);
-	uart::start();
+	s_uart0.init(9600);
+	s_uart0.start();
 }
 
 void uart_demoprint(const char *str)
 {
-	uart::print(str);
+	s_uart0.print(str);
 }
