4638a404d893d68d76f9780183b1e778274f09d9
Volodymyr Petrovskyy <pvvovan@gmail.com>
2025-01-04 17:01:03 +0100

    Build by cmake

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # UnoRtos
 
 Atmega328p freeRTOS demo app
+
+Build by ninja:
+cmake -S ./ -B ./cbuild/ -G Ninja -D CMAKE_TOOLCHAIN_FILE=./gccavr.cmake -D CMAKE_BUILD_TYPE=Release
+cmake --build ./cbuild/ --parallel --verbose
---
+++ gccavr.cmake
@@ -0,0 +1,30 @@
+set(CMAKE_SYSTEM_NAME Generic)
+
+set(CMAKE_C_COMPILER_FORCED TRUE)
+set(CMAKE_CXX_COMPILER_FORCED TRUE)
+set(CMAKE_ASM_COMPILER_FORCED TRUE)
+set(CMAKE_LINKER_FORCED TRUE)
+
+set(CMAKE_C_COMPILER_ID GNU)
+set(CMAKE_CXX_COMPILER_ID GNU)
+set(CMAKE_ASM_COMPILER_ID GNU)
+set(CMAKE_LINKER_ID GNU)
+
+set(CMAKE_C_COMPILER avr-gcc)
+set(CMAKE_CXX_COMPILER avr-g++)
+set(CMAKE_ASM_COMPILER avr-gcc)
+set(CMAKE_LINKER_ID avr-g++)
+
+set(CMAKE_EXECUTABLE_SUFFIX_C .elf)
+set(CMAKE_EXECUTABLE_SUFFIX_CXX .elf)
+set(CMAKE_EXECUTABLE_SUFFIX_ASM .elf)
+
+set(MCU atmega328p)
+
+add_compile_options(
+        -mmcu=${MCU}
+)
+
+add_link_options(
+        -mmcu=${MCU}
+)
