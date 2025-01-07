5055e4d8fea8edd3a233e45636becc335db72815
Volodymyr Petrovskyy <pvvovan@gmail.com>
2025-01-07 06:24:42 +0100

    Define CXX compile options in CMAKE_TOOLCHAIN_FILE

--- gccavr.cmake
+++ gccavr.cmake
@@ -13,7 +13,7 @@ set(CMAKE_LINKER_ID GNU)
 set(CMAKE_C_COMPILER avr-gcc)
 set(CMAKE_CXX_COMPILER avr-g++)
 set(CMAKE_ASM_COMPILER avr-gcc)
-set(CMAKE_LINKER_ID avr-g++)
+set(CMAKE_LINKER avr-g++)
 
 set(CMAKE_EXECUTABLE_SUFFIX_C .elf)
 set(CMAKE_EXECUTABLE_SUFFIX_CXX .elf)
@@ -21,6 +21,9 @@ set(CMAKE_EXECUTABLE_SUFFIX_ASM .elf)
 
 add_compile_options(
         -mmcu=${MCU}
+        $<$<COMPILE_LANGUAGE:ASM>:-x>
+        $<$<COMPILE_LANGUAGE:ASM>:assembler-with-cpp>
+        $<$<COMPILE_LANGUAGE:CXX>:-fno-exceptions>
 )
 
 add_link_options(
